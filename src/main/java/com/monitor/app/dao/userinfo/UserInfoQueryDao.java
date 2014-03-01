ef958b83368b0ea8b8727ef97db9d750bc28af37
djq1102 <djq1102@gmail.com>
2014-02-28 20:30:49 -0800

    用户管理 布袋

--- src/main/java/com/monitor/app/dao/userinfo/UserInfoQueryDao.java
+++ src/main/java/com/monitor/app/dao/userinfo/UserInfoQueryDao.java
@@ -3,11 +3,16 @@
  */
 package com.monitor.app.dao.userinfo;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.monitor.app.dataobject.UserInfo;
+import com.monitor.app.exception.DAOException;
 
 /**
  * @author Administrator
@@ -29,5 +34,32 @@ public interface UserInfoQueryDao {
 			  @Result(property = "gmtModify", column = "gmt_modify")
 	})
 	public UserInfo queryUserByNick(String nick);
+	
+	@Insert({"insert into cr_user_info (user_name,user_phone,user_email,login_name,password,customer_id,role_type,gmt_create,gmt_modify) "
+	  		+ "values (#{userName}, #{userPhone}, #{userEmail}, "
+	  		+ "#{loginName}, #{password}, #{customerId},#{roleType},now(),now()})"})
+	public void addUserInfo(UserInfo userInfo);
+	
+	@Update({"update cr_user_info set cr_user_info user_name=#{userName}#,user_phone=#{userPhone}#,user_email=#userEmail#,login_name=#loginName#"
+			+ "password=password,customer_id=customerId,role_type=roleType,gmt_modify=now()"})
+    public void update(UserInfo userInfo);
+    
+	@Results(value = { 
+			  @Result(id = true, property = "userId", column = "user_id"), 
+			  @Result(property = "userName", column = "user_name"),
+			  @Result(property = "userPhone", column = "user_phone"),
+			  @Result(property = "userEmail", column = "user_email"),
+			  @Result(property = "loginName", column = "login_name"),
+			  @Result(property = "password", column = "password"),
+			  @Result(property = "customerId", column = "customer_id"),
+			  @Result(property = "roleType", column = "role_type"),
+			  @Result(property = "gmtCreate", column = "gmt_create"),
+			  @Result(property = "gmtModify", column = "gmt_modify")
+	})
+	public List<UserInfo> selectAll();
+	
+	@Select("SELECT count(*) as total FROM cr_user_info")
+	int totalCount() throws DAOException;
+
 	
 }
