d25efdc58e78e5bcd160606bc49c0219ec5778b0
djq1102 <djq1102@gmail.com>
2014-02-28 22:58:34 -0800

    用户管理 布袋

--- src/main/java/com/monitor/app/dao/userinfo/UserInfoQueryDao.java
+++ src/main/java/com/monitor/app/dao/userinfo/UserInfoQueryDao.java
@@ -13,6 +13,7 @@ import org.apache.ibatis.annotations.Update;
 
 import com.monitor.app.dataobject.UserInfo;
 import com.monitor.app.exception.DAOException;
+import com.monitor.app.query.UserInfoQuery;
 
 /**
  * @author Administrator
@@ -35,15 +36,7 @@ public interface UserInfoQueryDao {
 	})
 	public UserInfo queryUserByNick(String nick);
 	
-	@Insert({"insert into cr_user_info (user_name,user_phone,user_email,login_name,password,customer_id,role_type,gmt_create,gmt_modify) "
-	  		+ "values (#{userName}, #{userPhone}, #{userEmail}, "
-	  		+ "#{loginName}, #{password}, #{customerId},#{roleType},now(),now()})"})
-	public void addUserInfo(UserInfo userInfo);
-	
-	@Update({"update cr_user_info set cr_user_info user_name=#{userName}#,user_phone=#{userPhone}#,user_email=#userEmail#,login_name=#loginName#"
-			+ "password=password,customer_id=customerId,role_type=roleType,gmt_modify=now()"})
-    public void update(UserInfo userInfo);
-    
+	@Select("SELECT * FROM cr_user_info WHERE user_id = #{userId}")
 	@Results(value = { 
 			  @Result(id = true, property = "userId", column = "user_id"), 
 			  @Result(property = "userName", column = "user_name"),
@@ -56,10 +49,34 @@ public interface UserInfoQueryDao {
 			  @Result(property = "gmtCreate", column = "gmt_create"),
 			  @Result(property = "gmtModify", column = "gmt_modify")
 	})
-	public List<UserInfo> selectAll();
+	public UserInfo queryUserInfoByUserId(long userId);
+	
+	@Insert({"insert into cr_user_info (user_name,user_phone,user_email,login_name,password,customer_id,role_type,gmt_create,gmt_modify) "
+	  		+ "values (#{userName}, #{userPhone}, #{userEmail}, "
+	  		+ "#{loginName}, #{password}, #{customerId},#{roleType},now(),now()})"})
+	public void addUserInfo(UserInfo userInfo);
+	
+	@Update({"update cr_user_info set cr_user_info user_name=#{userName}#,user_phone=#{userPhone}#,user_email=#userEmail#,login_name=#loginName#"
+			+ "password=password,customer_id=customerId,role_type=roleType,gmt_modify=now()"})
+    public void update(UserInfo userInfo);
+    
+	@Select("SELECT * FROM cr_user_info WHERE customer_id = #{customerId}")
+	@Results(value = { 
+			  @Result(id = true, property = "userId", column = "user_id"), 
+			  @Result(property = "userName", column = "user_name"),
+			  @Result(property = "userPhone", column = "user_phone"),
+			  @Result(property = "userEmail", column = "user_email"),
+			  @Result(property = "loginName", column = "login_name"),
+			  @Result(property = "password", column = "password"),
+			  @Result(property = "customerId", column = "customer_id"),
+			  @Result(property = "roleType", column = "role_type"),
+			  @Result(property = "gmtCreate", column = "gmt_create"),
+			  @Result(property = "gmtModify", column = "gmt_modify")
+	})
+	public List<UserInfo> queryUserInfo(UserInfoQuery userInfoQuery);
 	
 	@Select("SELECT count(*) as total FROM cr_user_info")
-	int totalCount() throws DAOException;
+	int totalCount(UserInfoQuery userInfoQuery) throws DAOException;
 
 	
 }
