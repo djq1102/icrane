77faa482f084165ea3248a221f377088fffec141
djq1102 <djq1102@gmail.com>
2014-03-08 22:29:36 -0800

    布袋 相关需求代码提交

--- src/main/java/com/monitor/app/CustomerController.java
+++ src/main/java/com/monitor/app/CustomerController.java
@@ -2,7 +2,12 @@ package com.monitor.app;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.monitor.app.service.CustomerService;
 
@@ -15,8 +20,15 @@ import com.monitor.app.service.CustomerService;
 @Controller
 public class CustomerController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
 	@Resource
 	private CustomerService customerService;
+	
+	@RequestMapping(value = "/customer/customerInput",method = RequestMethod.GET)
+	public String customerInput(Model model) {
+		logger.warn(">>>action=userInput");
+		return "customer/customer";
+	}
 	
 	
 
