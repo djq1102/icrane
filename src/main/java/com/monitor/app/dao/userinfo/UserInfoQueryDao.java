9e4d1057f6a8f56ba788ce4070306007ddf3737a
djq1102 <djq1102@gmail.com>
2014-03-08 21:06:07 -0800

    布袋 相关需求代码提交

--- src/main/java/com/monitor/app/dao/userinfo/UserInfoQueryDao.java
+++ src/main/java/com/monitor/app/dao/userinfo/UserInfoQueryDao.java
@@ -5,10 +5,6 @@ package com.monitor.app.dao.userinfo;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Result;
-import org.apache.ibatis.annotations.Results;
-import org.apache.ibatis.annotations.Select;
-
 import com.monitor.app.dataobject.UserInfo;
 import com.monitor.app.exception.DAOException;
 import com.monitor.app.query.UserInfoQuery;
@@ -19,19 +15,6 @@ import com.monitor.app.query.UserInfoQuery;
  */
 public interface UserInfoQueryDao {
 
-	@Select("SELECT * FROM cr_user_info WHERE login_name = #{nick}")
-	@Results(value = { 
-			  @Result(id = true, property = "userId", column = "user_id"), 
-			  @Result(property = "userName", column = "user_name"),
-			  @Result(property = "userPhone", column = "user_phone"),
-			  @Result(property = "userEmail", column = "user_email"),
-			  @Result(property = "loginName", column = "login_name"),
-			  @Result(property = "password", column = "password"),
-			  @Result(property = "customerId", column = "customer_id"),
-			  @Result(property = "roleType", column = "role_type"),
-			  @Result(property = "gmtCreate", column = "gmt_create"),
-			  @Result(property = "gmtModify", column = "gmt_modify")
-	})
 	public UserInfo queryUserByNick(String nick);
 	
 	public UserInfo queryUserInfoByUserId(long userId);
