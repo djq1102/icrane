6c618e4545a8c9ff9dea9a03f6858eb4e808cb20
djq1102 <djq1102@gmail.com>
2014-03-30 14:07:18 +0800

    fi model

--- src/main/java/com/monitor/app/query/PlcModelQuery.java
+++ src/main/java/com/monitor/app/query/PlcModelQuery.java
@@ -4,4 +4,25 @@ import com.monitor.app.dao.common.Pagination;
 
 public class PlcModelQuery extends Pagination{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2567930026451206635L;
+	private long modelId;
+	private String modelName;
+	
+	public long getModelId() {
+		return modelId;
+	}
+	public void setModelId(long modelId) {
+		this.modelId = modelId;
+	}
+	public String getModelName() {
+		return modelName;
+	}
+	public void setModelName(String modelName) {
+		this.modelName = modelName;
+	}
+	
+	
 }
